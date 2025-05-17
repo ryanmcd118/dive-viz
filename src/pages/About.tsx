79c00420d6259d6004cae707fe384dd0a8c77a85
Ryan McDaniel <rp.mcdan@gmail.com>
2025-05-17 11:07:23 -0400

    Add simple coming soon landing page

--- src/pages/About.tsx
+++
@@ -1,3 +0,0 @@
-export default function About() {
-  return <p>This app helps you plan and log scuba dives like a pro.</p>;
-}
--- src/pages/DiveLog.tsx
+++
@@ -1,3 +0,0 @@
-export default function DiveLog() {
-  return <h1 className='text-2xl font-semibold'>Your Dive Log</h1>;
-}
