e8cd0b070815af21d5d9f4cb56385c7a4cffec17
Ryan McDaniel <rp.mcdan@gmail.com>
2025-05-16 20:48:49 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='min-h-screen bg-gradient-to-br from-sky-400 to-blue-800 flex items-center justify-center'>
+      <h1 className='text-4xl font-bold text-white drop-shadow-lg'>
+        🌊 DiveViz is alive!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
